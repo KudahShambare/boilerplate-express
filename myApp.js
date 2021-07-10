fc50687e31f645280cc7a22e6f9932c010dc0544
KudahShambare <kudahshambare263@gmail.com>
2021-07-11 00:19:23 +0200

    Hello World Done

--- myApp.js
+++ myApp.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.get("/", (request, response) => {
+    response.send("Hello World")
+})
 
+let PORT = 5000;
 
+app.listen(PORT, () => {
+    console.log("Listening to port "+PORT);
+})
 
 
 
